850c18a9db91d2217e2c5a3a30eb8ee501691c69
Denis Konev <deniskonev1984@gmail.com>
2024-10-11 16:08:45 +0300

    Создан скрипт наполнения БД тестовыми данными

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (first_name, last_name, middle_name, date_of_birth, email, phone_number, photo)
+VALUES ('Иван', 'Иванов', 'Иванович', '1990-01-01', 'ivan.ivanov@example.com', '+79001234567', NULL),
+       ('Петр', 'Петров', 'Петрович', '1985-05-15', 'petr.petrov@example.com', '+79007654321', NULL),
+       ('Светлана', 'Сидорова', NULL, '1992-07-20', 'svetlana.sidorova@example.com', '+79005553322', NULL);
