3619c15288dd408f992f5a08b276c4426421cdfd
Denis Konev <deniskonev1984@gmail.com>
2024-10-11 13:59:22 +0300

    Настроена конфигурация базы данных

--- src/main/java/io/github/deniskonev/usermanagementapi/UserManagementApiApplication.java
+++ src/main/java/io/github/deniskonev/usermanagementapi/UserManagementApiApplication.java
@@ -2,8 +2,10 @@ package io.github.deniskonev.usermanagementapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:db.properties")
 public class UserManagementApiApplication {
 
     public static void main(String[] args) {
