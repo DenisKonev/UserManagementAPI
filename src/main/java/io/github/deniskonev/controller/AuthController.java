65b8238ea5f31855a854196c81b4bf1bb15cb5d3
Denis Konev <deniskonev1984@gmail.com>
2024-10-11 16:16:11 +0300

    Рефакторинг JWT контроллера

--- src/main/java/io/github/deniskonev/controller/AuthController.java
+++ src/main/java/io/github/deniskonev/controller/AuthController.java
@@ -3,22 +3,16 @@ package io.github.deniskonev.controller;
 import io.github.deniskonev.dto.AuthRequest;
 import io.github.deniskonev.security.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Map;
 
 @RestController
 public class AuthController {
-
-    @Autowired
-    private AuthenticationManager authenticationManager;
 
     @Autowired
     private JwtUtils jwtUtils;
@@ -26,16 +20,7 @@ public class AuthController {
     @PostMapping("/authenticate")
     public Map<String, String> authenticate(@RequestBody AuthRequest authRequest) {
         try {
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            authRequest.getUsername(), authRequest.getPassword()));
-
-            String token = jwtUtils.generateJwtToken(authRequest.getUsername());
-
-            Map<String, String> response = new HashMap<>();
-            response.put("token", token);
-            return response;
-
+            return Collections.singletonMap("token", jwtUtils.generateJwtToken(authRequest.getUsername()));
         } catch (AuthenticationException e) {
             throw new RuntimeException("Invalid credentials");
         }
